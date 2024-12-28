4b9d953ffdb49b689864e6ce30dfe08e5087c880
kamalijenifer05 <kamalijenifer05@gmail.com>
2024-12-28 11:53:58 +0530

    comit

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styled from "styled-components";
 import axios from "axios";
 import { useRouter } from "next/navigation";
